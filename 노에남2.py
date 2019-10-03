d12fd4bbcf17486d287c4e48cc634db259b05d40
hyuuun-tt <56111347+hyuuun-tt@users.noreply.github.com>
2019-10-04 02:27:13 +0900

    Update 노에남2.py

--- 노에남2.py
+++ 노에남2.py
@@ -92,7 +92,16 @@ async def on_message(message):
     if message.content.startswith("살남사건이야"):
         await message.channel.send(":tada: 축살남 :tada: ")
     if message.content.startswith("닥쳐"):
-        await message.channel.send("냅!:zipper_mouth: :zipper_mouth: ")
+        await message.channel.send("냅!! :zipper_mouth: :zipper_mouth: ")
+    if message.content.startswith("노예남 후장"):
+        await message.channel.send("제 후장은 왜...요..?? :no_good:")
+     if message.content.startswith("너 매미니스트지"):
+        await message.channel.send("매..매미니즘이 모 어때서욧..!! :rage: ")
+     if message.content.startswith("웅웅 알았어요"):
+        await message.channel.send("역시 주인님은 제 맘을 알아줄줄 알았어요 :sob: 오늘 상다리 부서지게 밥 차려드릴게요! :kissing_heart: ")
+        
+        
+    
         
         
 
